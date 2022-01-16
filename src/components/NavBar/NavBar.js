6b7e9312f2c12771bd46fb04b6bb29e1e6fd9dd6
Tamara Salinas <gatamara29@hmail.com>
2022-01-15 23:34:21 -0300

    segundo desafio

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+
+export function NavBar () {
+    return(
+        <div>
+            <header className="header">
+                <h1>Morron y Cuenta Nueva</h1>
+              
+              <nav >
+                  <ul >
+                      <li ><a href="./">Inicio</a> </li>
+                      <li ><a href="./">Menu</a></li>
+                      <li ><a href="./">Galeria</a></li>
+                      <li ><a href="./">Nosotros</a></li>
+                      <li ><a href="./">Contacto</a></li>
+                  </ul>
+              </nav>
+          </header>
+        </div>
+    )
+  }
+
+  
